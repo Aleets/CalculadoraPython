a60764c7a3866a6005b7faddddf12a89e878cf42
Aleets <alex_26_azul@hotmail.com>
2022-09-28 10:17:30 -0500

    Se agrega calculadora 3.0

--- Calculator.py
+++ Calculator.py
@@ -18,25 +18,29 @@ def divi(num1, num2):
 def return_values():
   num1 = int(input("Ingresa tu primer valor: "))
   num2 = int(input("Ingresa tu sugundo valor: "))
-  return 
+  return [num1, num2]
 
 if __name__ == '__main__':
     message = f"Calculadora: \n Elige una opcion \n 1 - suma \n 2 - resta \n 3 - multiplicacion \n 4 - divición  \n"
     while True:
       opcion = int(input(message))
       if opcion ==1:
-        result_sum = sum (23, 54)
+        numeros = return_values()
+        result_sum = sum(numeros[0], numeros[1])
         print("El resultado de la suma es: ", result_sum)
       elif opcion ==2:
-        result_rest = rest(23, 54)
+        numeros = return_values()
+        result_rest = rest(numeros[0], numeros[1])
         print("El resultado de la resta es: ", result_rest)
-      if opcion ==3:
-        result_multi = multiplication(23, 54)
+      elif opcion ==3:
+        numeros = return_values()
+        result_multi = multiplication(numeros[0], numeros[1])
         print("El resultado de la multiplicacion es: ",result_multi)
-      if opcion == 4:
-        result_div = divi(23, 54)
+      elif opcion == 4:
+        numeros = return_values()
+        result_div = divi(numeros[0], numeros[1])
         print("El resultado de la divicion es: ",result_div)
-      if opcion == 5:
+      elif opcion == 5:
         print("Bye!")
         break
       else:
